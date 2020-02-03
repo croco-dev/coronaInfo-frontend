966428f1d6b1e570c238d7c96ceceb125c807fa7
DDark <ddark.kr@gmail.com>
2020-02-03 22:39:56 +0900

    feat: ⚡️ 모바일 대응

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,15 +1,50 @@
-import React from 'react'
-import Header from '@/layouts/components/Header'
+import React, { useState, useEffect } from 'react'
+import dynamic from 'next/dynamic'
 import Footer from '@/layouts/components/Footer'
 
 const MainLayout: React.FC = props => {
-  return (
-    <>
-      <Header />
-      {props.children}
-      <Footer />
-    </>
-  )
+  const [isMobile, setIsMobile] = useState(false) // 모바일 여부
+  const [sidebar, setSidebar] = useState(false) // 사이드바 On/Off
+
+  const resizeEvent = () => {
+    if (window.innerWidth < 992) {
+      setIsMobile(true)
+    } else {
+      setIsMobile(false)
+    }
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      resizeEvent()
+      window.addEventListener('resize', resizeEvent)
+      return () => {
+        window.removeEventListener('resize', resizeEvent)
+      }
+    }
+  }, [])
+
+  if (isMobile) {
+    // mobile mode
+    const HeaderMobile = dynamic(() => import('@/layouts/components/HeaderMobile'))
+    return (
+      <>
+        <HeaderMobile />
+        {props.children}
+        <Footer />
+      </>
+    )
+  } else {
+    // desktop mode
+    const Header = dynamic(() => import('@/layouts/components/Header'))
+    return (
+      <>
+        <Header />
+        {props.children}
+        <Footer />
+      </>
+    )
+  }
 }
 
 export default MainLayout
