9a5d9874e0a570c7142df663922dddc32538dbfa
DDark <ddark.kr@gmail.com>
2020-02-03 21:37:45 +0900

    feat: ⚡️ Main Layout

---
+++ src/layouts/components/Header.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import Container from '@/components/Container'
+
+const EditedContainer = styled(Container)`
+  align-items: center;
+  display: flex;
+  height: 100%;
+`
+
+const Nav = styled.nav`
+  background: #fff;
+  height: 60px;
+`
+
+const NavLogo = styled.a`
+  font-size: 1.3rem;
+  span {
+    color: var(--main);
+    font-weight: 700;
+  }
+`
+
+const NavMenu = styled.ul`
+  align-items: center;
+  display: flex;
+  list-style: none;
+  margin-left: 20px;
+  li {
+    padding: 0 13px;
+    font-size: 0.95rem;
+  }
+`
+
+const NavRight = styled.div`
+  display: block;
+  margin-left: auto;
+`
+
+const Header: React.FC = () => {
+  return (
+    <Nav>
+      <EditedContainer>
+        <NavLogo>
+          <span>코로나</span>인포
+        </NavLogo>
+        <NavMenu>
+          <li>지도</li>
+          <li>영상 모아보기</li>
+          <li>실시간 피드</li>
+          <li>커뮤니티 게시판</li>
+        </NavMenu>
+        <NavRight>Right</NavRight>
+      </EditedContainer>
+    </Nav>
+  )
+}
+
+export default Header
---
+++ src/layouts/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '@/layouts/components/Header'
+
+const MainLayout: React.FC = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default MainLayout
