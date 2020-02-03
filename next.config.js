f82494d2d88943f94e31bad493399ca2b65ae2ff
DDark <ddark.kr@gmail.com>
2020-02-03 20:51:32 +0900

    feat: ⚡️ CSS Minified

--- next.config.js
+++ next.config.js
@@ -3,13 +3,22 @@ const withSass = require('@zeit/next-sass')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = withSass({
+  poweredByHeader: false,
   webpack: (config, options) => {
+    // tsconfig-paths
     if (config.resolve.plugins) {
       config.resolve.plugins.push(new TsconfigPathsPlugin())
     } else {
       config.resolve.plugins = [new TsconfigPathsPlugin()]
     }
 
+    // CSS Minified
+    config.optimization.minimizer = []
+    config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}))
+
     return config
+  },
+  devIndicators: {
+    autoPrerender: false,
   },
 })
