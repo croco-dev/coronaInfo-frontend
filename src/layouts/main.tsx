8492a2ac3e37c11690bbbd51c07b53c5c1ad4a25
DDark <ddark.kr@gmail.com>
2020-02-03 22:09:31 +0900

    feat: ⚡️ 메인 레이아웃 완성

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from '@/layouts/components/Header'
+import Footer from '@/layouts/components/Footer'
 
-const MainLayout: React.FC = () => {
+const MainLayout: React.FC = props => {
   return (
     <>
       <Header />
+      {props.children}
+      <Footer />
     </>
   )
 }
