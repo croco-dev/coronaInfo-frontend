8dda1cf53d5621223571ed805969b6edc139130c
DDark <ddark.kr@gmail.com>
2020-02-07 03:29:26 +0900

    feat: ⚡️ API 받음.
    UI/UX 개선 필요.

--- src/pages/feeds.tsx
+++ src/pages/feeds.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from '@emotion/styled'
 import Layout from '@/layouts/main'
 import Container from '@/components/Container'
+import fetch from 'isomorphic-unfetch'
 
 const Jumbotron = styled.div`
   background: #f1f1f1;
@@ -16,7 +17,59 @@ const Jumbotron = styled.div`
   }
 `
 
-const FeedPage: React.FC = () => {
+const DataBox = styled.div`
+  padding: 25px 0;
+`
+
+const CardBox = styled.div`
+  border: 1px solid #e8e8e8;
+  padding: 20px 15px;
+  margin: 25px 0;
+  .raw {
+    margin-top: 15px;
+    display: block;
+    background: #ececec;
+    padding: 10px 20px;
+  }
+`
+
+const FeedPage = ({ data }): JSX.Element => {
+  const DataShow = (): JSX.Element => {
+    return (
+      <>
+        <DataBox>
+          {data.map((row, i) => {
+            return <Card data={row} key={i} />
+          })}
+        </DataBox>
+      </>
+    )
+  }
+
+  const Card = ({ data }): JSX.Element => {
+    return (
+      <>
+        <CardBox>
+          {data.date}, {data.log_type} 가 변경되었습니다.
+          {data.place ? <span className="raw">{data.place}</span> : <></>}
+        </CardBox>
+      </>
+    )
+  }
+
+  const DataLoading = (): JSX.Element => (
+    <>
+      <div
+        style={{
+          padding: '20px 0px',
+          textAlign: 'center',
+        }}
+      >
+        <p>데이터가 없습니다.</p>
+      </div>
+    </>
+  )
+
   return (
     <>
       <Layout>
@@ -28,9 +81,25 @@ const FeedPage: React.FC = () => {
             </p>
           </Container>
         </Jumbotron>
+        {data && data.length > 0 ? (
+          <>
+            <Container>
+              <DataShow />
+            </Container>
+          </>
+        ) : (
+          <DataLoading />
+        )}
       </Layout>
     </>
   )
+}
+
+FeedPage.getInitialProps = async () => {
+  const res = await fetch('https://api.coronas.info/feeds/?format=json')
+  const json = await res.json()
+  console.log(json)
+  return { data: json }
 }
 
 export default FeedPage
