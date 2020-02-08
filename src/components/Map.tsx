7c72413d4ca1fefd2b169c2d8d9313296a53b57c
kang-heewon <itphantom@icloud.com>
2020-02-08 15:32:55 +0900

    feat:⚡️ add marker

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,25 +1,38 @@
 import React, { useRef, useEffect } from 'react'
-import { RenderAfterNavermapsLoaded, NaverMap } from 'react-naver-maps'
+import { RenderAfterNavermapsLoaded, NaverMap, Marker } from 'react-naver-maps'
 
 const naverMapClientId = process.env.NAVER_MAP_API || ''
 declare const naver: any
+declare global {
+  interface Window {
+    naver: any
+  }
+}
 
-const MapComponent: React.FC = () => {
+const Map = ({ movements }): JSX.Element => {
   return (
     <>
-      <RenderAfterNavermapsLoaded ncpClientId={naverMapClientId}>
-        <NaverMap
-          mapDivId={'dash-map'} // default: react-naver-map
-          style={{
-            width: '100%',
-            height: '45vh',
-          }}
-          defaultCenter={{ lat: 37.3213564, lng: 127.0978459 }}
-          defaultZoom={8}
-        />
-      </RenderAfterNavermapsLoaded>
+      <NaverMap
+        mapDivId={'dash-map'} // default: react-naver-map
+        style={{
+          width: '100%',
+          height: '45vh',
+        }}
+        defaultCenter={{ lat: 37.3213564, lng: 127.0978459 }}
+        defaultZoom={8}
+      >
+        {movements.map(item => {
+          const navermaps = window.naver.maps
+          return <Marker position={new navermaps.LatLng(item.lat, item.lng)} title={item.place} />
+        })}
+      </NaverMap>
     </>
   )
 }
+const MapComponent = ({ movements }): JSX.Element => (
+  <RenderAfterNavermapsLoaded ncpClientId={naverMapClientId}>
+    <Map movements={movements} />
+  </RenderAfterNavermapsLoaded>
+)
 
 export default MapComponent
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import Layout from '@/layouts/main'
 import dynamic from 'next/dynamic'
+import fetch from 'isomorphic-unfetch'
 
 const Map = dynamic(() => import('@/components/Map'), { ssr: false })
 
-const Home: React.FC = () => {
+const Home = ({ data }): JSX.Element => {
   return (
     <Layout>
-      <Map />
+      <Map movements={data} />
     </Layout>
   )
+}
+
+Home.getInitialProps = async () => {
+  const res = await fetch(`${process.env.API_URL}/movements/?format=json`)
+  const json = await res.json()
+  return { data: json }
 }
 
 export default Home
