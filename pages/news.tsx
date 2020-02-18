bc3ab8e33fbd9aad17ab1d21104e5b0b435109b8
DDark <ddark.kr@gmail.com>
2020-02-18 23:33:46 +0900

    feat: ⚡️ News

--- pages/news.tsx
+++ pages/news.tsx
@@ -6,10 +6,53 @@ import Container from '@/components/Container'
 import Jumbotron from '@/components/Jumbotron'
 
 const Content = styled.div`
-  padding: 20px 0;
+  padding: 30px 0;
+  background: #fff;
 `
 
-const NewsPage = (): JSX.Element => {
+const CardContainer = styled.div``
+
+const Card = styled.a`
+  display: block;
+  border-radius: 6px;
+  border: 1px solid #dadce0;
+  padding: 20px 17px;
+  margin: 15px 0;
+  line-height: 1.4;
+  text-decoration: none;
+
+  h3 {
+    color: #333;
+    font-size: 1.2rem;
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+    }
+  }
+  p {
+    color: #757575;
+    font-size: 0.83rem;
+    margin-top: 7px;
+  }
+`
+
+const NewsPage = ({ data }): JSX.Element => {
+  const DataCard = ({ data }): JSX.Element => {
+    const date = new Date(data.pubDate)
+    return (
+      <Card href={data.originallink}>
+        <h3 dangerouslySetInnerHTML={{ __html: data.title }}></h3>
+        <p dangerouslySetInnerHTML={{ __html: data.description }}></p>
+      </Card>
+    )
+  }
+
+  const News = () => {
+    data.map((row, i) => {
+      return <DataCard data={row} key={i} />
+    })
+  }
+
   return (
     <>
       <Head>
@@ -21,11 +64,23 @@ const NewsPage = (): JSX.Element => {
           desc="'코로나19'에 관련된 뉴스 기사를 모아보실 수 있습니다."
         />
         <Content>
-          <Container></Container>
+          <Container>
+            <CardContainer>
+              {data.news.map((row, i) => {
+                return <DataCard data={row} key={i} />
+              })}
+            </CardContainer>
+          </Container>
         </Content>
       </Layout>
     </>
   )
+}
+
+NewsPage.getInitialProps = async () => {
+  const res = await fetch(`${process.env.API_URL}/news/?format=json`)
+  const json = await res.json()
+  return { data: json }
 }
 
 export default NewsPage
