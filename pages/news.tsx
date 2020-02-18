39bf2f4b88c85f0e004e5658778d69a52fa6976f
DDark <ddark.kr@gmail.com>
2020-02-18 22:37:00 +0900

    style: 👁 Content

--- pages/news.tsx
+++ pages/news.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import Head from 'next/head'
 import styled from '@emotion/styled'
 import Layout from '@/layouts/main'
+import Container from '@/components/Container'
 import Jumbotron from '@/components/Jumbotron'
+
+const Content = styled.div`
+  padding: 20px 0;
+`
 
 const NewsPage = (): JSX.Element => {
   return (
@@ -15,6 +20,9 @@ const NewsPage = (): JSX.Element => {
           title="기사 모아보기"
           desc="'코로나19'에 관련된 뉴스 기사를 모아보실 수 있습니다."
         />
+        <Content>
+          <Container></Container>
+        </Content>
       </Layout>
     </>
   )
