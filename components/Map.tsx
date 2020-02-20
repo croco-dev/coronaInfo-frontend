cc52adfa61169903e146ed6e9662a20ba2308221
DDark <ddark.kr@gmail.com>
2020-02-20 23:20:46 +0900

    feat: ⚡️ 마커 클러스터 적용...!
    힘들었다 🤤

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,10 +1,14 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { RenderAfterNavermapsLoaded, NaverMap, Marker } from 'react-naver-maps'
+import styled from '@emotion/styled'
 
 declare global {
   interface Window {
     naver: any
   }
+  const naver: any
+  const MarkerClustering: any
+  const N: any
 }
 
 interface MapProps {
@@ -12,35 +16,73 @@ interface MapProps {
   height?: string
 }
 
-const Map: React.FC<MapProps> = ({ movements, height }): JSX.Element => {
-  const navermaps = window.naver.maps
-  return (
-    <>
-      <NaverMap
-        mapDivId={'dash-map'} // default: react-naver-map
-        style={{
-          width: '100%',
-          height: height || '40vh',
-        }}
-        defaultCenter={{ lat: 36.3213564, lng: 127.0978459 }}
-        defaultZoom={6}
-      >
-        {movements.map((item, i) => {
-          return (
-            <Marker
-              key={i}
-              position={new navermaps.LatLng(item.lat, item.lng)}
-              title={item.place}
-            />
-          )
-        })}
-      </NaverMap>
-    </>
-  )
-}
-const MapComponent = ({ movements }): JSX.Element => {
-  if (window.naver.maps) {
-    return <Map movements={movements} />
+const MapWrapper = styled.div`
+  .clusterMark {
+    cursor: pointer;
+    width: 25px;
+    height: 25px;
+    line-height: 26px;
+    font-size: 10px;
+    border-radius: 50%;
+    color: white;
+    text-align: center;
+    font-weight: bold;
+    background: rgba(195, 97, 255, 0.8);
+    background-size: contain;
+  }
+`
+
+class MapComponent extends React.Component<MapProps> {
+  mapRef: any
+
+  render(): JSX.Element {
+    return (
+      <MapWrapper>
+        <NaverMap
+          naverRef={ref => {
+            this.mapRef = ref
+          }}
+          mapDivId={'dash-map'} // default: react-naver-map
+          style={{
+            width: '100%',
+            height: '40vh',
+          }}
+          defaultCenter={{ lat: 36.3213564, lng: 127.0978459 }}
+          defaultZoom={6}
+        ></NaverMap>
+      </MapWrapper>
+    )
+  }
+
+  componentDidMount(): void {
+    const markers = []
+    this.props.movements.map((item, i) => {
+      const marker = new naver.maps.Marker({
+        position: new naver.maps.LatLng(item.lat, item.lng),
+        map: this.mapRef.instance,
+      })
+      markers.push(marker)
+    })
+
+    const htmlMarker1 = {
+      content: '<div class="clusterMark"></div>',
+      size: N.Size(40, 40),
+      anchor: N.Point(20, 20),
+    }
+
+    const markerClustering = new MarkerClustering({
+      minClusterSize: 1,
+      maxZoom: 8,
+      map: this.mapRef.instance,
+      markers: markers,
+      disableClickZoom: false,
+      gridSize: 120,
+      icons: [htmlMarker1],
+      indexGenerator: [10],
+      stylingFunction: function(clusterMarker, count) {
+        clusterMarker._wrapper.firstChild.innerText = count
+      },
+    })
   }
 }
 
