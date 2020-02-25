b1edf8b18f488fc1345395cefe4dc2edd9ca1b6f
DDark <ddark.kr@gmail.com>
2020-02-26 01:50:25 +0900

    feat: ⚡️ 확진자 통계

--- layouts/components/Menu.ts
+++ layouts/components/Menu.ts
@@ -4,4 +4,5 @@ export const Menu = [
   // { title: '영상 모아보기', href: '/videos' },
   { title: '실시간 피드', href: '/feeds' },
   { title: '실시간 뉴스', href: '/news' },
+  { title: '확진자 통계', href: '/statistics' },
 ]
