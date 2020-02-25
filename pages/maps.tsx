c4e2cabb7ec2618977e1fb86a91959addb12f0d9
DDark <ddark.kr@gmail.com>
2020-02-26 02:12:03 +0900

    feat: ⚡️ maps -> movement Redirection

---
+++ pages/maps.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Maps = () => {
+  return <></>
+}
+
+Maps.getInitialProps = ({ res }) => {
+  if (res) {
+    // Server
+    res.writeHead(301, {
+      Location: 'https://coronas.info/movement',
+    })
+    res.end()
+  }
+
+  return {}
+}
