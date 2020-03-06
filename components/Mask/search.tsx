fdbab82c933c86e4a9121e57e565ef6e2619d7e2
도다 (@ddarkr) <ddark.kr@gmail.com>
2020-03-06 22:33:48 +0900

    feat: ⚡ SearchForm

--- components/Mask/search.tsx
+++ components/Mask/search.tsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import Card from '@/components/Card'
+import { InlineIcon } from '@iconify/react'
+import bxSearch from '@iconify/icons-bx/bx-search'
 
-const MaskSearch = () => {
+const SearchForm = styled.form`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  background: #efefef;
+  input {
+    border: 0;
+    background: transparent;
+    outline: none;
+    padding: 20px 25px;
+    font-size: 18px;
+    width: 100%;
+    -webkit-appearance: none;
+  }
+  button {
+    -webkit-appearance: none;
+    background: transparent;
+    border: 0;
+    outline: none;
+    padding: 20px;
+    svg {
+      width: 20px;
+      height: 20px;
+    }
+  }
+`
+
+const MaskSearch = (): JSX.Element => {
   return (
     <>
-      <div></div>
+      <Card>
+        <SearchForm>
+          <input />
+          <button>
+            <InlineIcon icon={bxSearch} />
+          </button>
+        </SearchForm>
+      </Card>
     </>
   )
 }
