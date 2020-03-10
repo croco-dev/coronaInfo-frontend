4e3ca4dd8f70604615ec7044245391a038f501bd
DDark <ddark.kr@gmail.com>
2020-03-10 20:40:30 +0900

    feat: maskMap 페이지 만듬

--- pages/maskmap.tsx
+++ pages/maskmap.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { RenderAfterNavermapsLoaded, NaverMap, Polyline } from 'react-naver-maps'
+import Layout from '@/layouts/main'
 
 const MaskMap = (): JSX.Element => {
-  return <></>
+  return (
+    <>
+      <Layout>
+        {window && (
+          <NaverMap
+            mapDivId={'dash-map'} // default: react-naver-map
+            style={{
+              width: '100%',
+              height: '100vh',
+            }}
+            defaultCenter={{ lat: 37.3213564, lng: 127.0978459 }}
+            defaultZoom={8}
+          ></NaverMap>
+        )}
+      </Layout>
+    </>
+  )
 }
 
 export default MaskMap
