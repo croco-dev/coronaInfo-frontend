2f9ab41e8d38fb1781e838e5f8687f4ed7bf9a1e
DDark <ddark.kr@gmail.com>
2020-03-10 20:40:35 +0900

    feat: 나중에 쓸 함수

--- components/Mask/card.tsx
+++ components/Mask/card.tsx
@@ -2,6 +2,22 @@ import React, { useState } from 'react'
 import styled from '@emotion/styled'
 import Card from '@/components/Card'
 
+// const calDistance = (lat1, lng1, lat2, lng2) => {
+//   const deg2rad = deg => {
+//     return deg * (Math.PI / 180)
+//   }
+
+//   const R = 6371 // Radius of the earth in km
+//   const dLat = deg2rad(lat2 - lat1) // deg2rad below
+//   const dLon = deg2rad(lng2 - lng1)
+//   const a =
+//     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+//     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2)
+//   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+//   const d = R * c // Distance in km
+//   return d
+// }
+
 const StyleSection = styled.div`
   line-height: 1.5;
   .gray {
